9b914cf6b9a5eae63b8811382038aa438cbc7c56
Gina <gina@ginazampino.com>
2018-08-28 18:44:23 -0400

    Added color and texture logic.

--- index.js
+++ index.js
@@ -5,13 +5,29 @@ function clearEditor() {
 };
 
 function anchorAll() {
+    var color = document.getElementById('color').value;
+    var texture = document.getElementById('texture').value;
     var editor = document.getElementById('editor');
+ 
+    if (editor.value) {
+        var paintBallz = editor.value.split('\n');
 
-    if (editor.value != '') {
-        var lines = editor.value.split('\n');
+        var splitArray = paintBallz.map(function (item) {
+            return item.split(',');
+        });
 
-        var anchoredArray = lines.map(function (item) {
-            return item.concat(',\t0');
+        var colorStep = !color ? splitArray : splitArray.map(function (item) {
+            item[5] = item[5].replace(/-?\d+/, color);
+            return item;
+        });
+
+        var textureStep = !texture ? colorStep : colorStep.map(function (item) {
+            item[item.length -1] = item[item.length -1].replace(/-?\d+/, texture);
+            return item;
+        });
+
+        var anchoredArray = textureStep.map(function (item) {
+            return item.concat('\t0');
         });
 
         var anchoredString = anchoredArray.join('\n');
